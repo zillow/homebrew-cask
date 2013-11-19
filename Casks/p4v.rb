a606749836850bb7113c946d236ea368efb25b31
Daniel Stockman <daniels@zillow.com>
2013-11-19 13:33:30 -0800

    Add p4v cask.

---
+++ Casks/p4v.rb
@@ -0,0 +1,8 @@
+class P4v < Cask
+  url 'http://filehost.perforce.com/perforce/r10.1/bin.macosx104u/P4V.dmg'
+  homepage 'http://www.perforce.com/product/components/perforce-clients'
+  version '10.1'
+  sha1 '4d143bfac010a38e29c73dadc99a79e24413bff9'
+  link 'p4v.app'
+  link 'p4merge.app'
+end
